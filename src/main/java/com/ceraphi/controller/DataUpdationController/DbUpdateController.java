f9522fe08b36e69971ad2679eae193681b89f786
muneendra7 <muneendra@appservinc.com>
2023-11-20 11:34:17 +0530

    10/10/23

--- src/main/java/com/ceraphi/controller/DataUpdationController/DbUpdateController.java
+++ src/main/java/com/ceraphi/controller/DataUpdationController/DbUpdateController.java
@@ -3,9 +3,11 @@ package com.ceraphi.controller.DataUpdationController;
 import com.ceraphi.dto.MasterDataTablesDto.*;
 import com.ceraphi.dto.ProDataBaseModelDto;
 import com.ceraphi.dto.WellInfoDto;
+import com.ceraphi.entities.MasterDataTables.ProDataBaseModel;
 import com.ceraphi.services.Impl.DbUpdateService;
 import com.ceraphi.utils.ApiResponseData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,76 +18,101 @@ import java.util.List;
 public class DbUpdateController {
     @Autowired
     private DbUpdateService dbUpdateService;
+//    @PostMapping("/newProDataRecord")
+//    public ResponseEntity<?> addNewRecordData(@RequestBody ProDataBaseModelDto proDataBaseModelDto) {
+//        dbUpdateService.addNewData(proDataBaseModelDto);
+//        return ResponseEntity.ok("Added successfully");
+//    }
 
     @PutMapping("/proDataUpdate")
-    public ResponseEntity<?> updateData(@RequestBody ProDataBaseModelDto proDataBaseModelDto) {
-        dbUpdateService.updateExistingData(proDataBaseModelDto);
-        return ResponseEntity.ok("update successfully");
-    }
+    public ResponseEntity<?> updateRecords(@RequestBody List<ProDataBaseModelDto> updatedModels) {
+        dbUpdateService.updateRecords(updatedModels);
 
+      ApiResponseData<?> response = ApiResponseData.builder().message("update successfully").status(HttpStatus.OK.value()).build();
+        return ResponseEntity.ok(response);
+    }
+    @GetMapping("/revert/{changeSetId}")
+    public ResponseEntity<?> revertChangeSet(@PathVariable Long changeSetId) {
+        dbUpdateService.revertChangeSet(changeSetId);
+        ApiResponseData<?> response = ApiResponseData.builder().message("restored successfully").status(HttpStatus.OK.value()).build();
+        return ResponseEntity.ok(response);
+
+    }
     @GetMapping("/ProDataList")
-    public ResponseEntity<List<ProDataBaseModelDto>> getDataList() {
+    public ResponseEntity<?> getDataList() {
         List<ProDataBaseModelDto> theDataList = dbUpdateService.getTheDataList();
-        return ResponseEntity.ok(theDataList);
+        ApiResponseData<?> response = ApiResponseData.builder().data(theDataList).message("restored successfully").status(HttpStatus.OK.value()).build();
+        return ResponseEntity.ok(response);
     }
 
 
     @PutMapping("/estCostCapexDeep")
     public ResponseEntity<?> estimated_cost_Capex_Deep(@RequestBody EstimatedCostCapexDeepDto estimatedCostCapexDeepDto) {
         dbUpdateService.estimated_cost_Capex_Deep(estimatedCostCapexDeepDto);
-        return ResponseEntity.ok("update successfully");
+        ApiResponseData<?> response = ApiResponseData.builder().message("update successfully").status(HttpStatus.OK.value()).build();
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/getEstCostCapexDeepList")
     public ResponseEntity<?> getTheDataListEst_Cost_Capex_Deep() {
         List<EstimatedCostCapexDeepDto> theDataList = dbUpdateService.getTheDataListEst_Cost_Capex_Deep();
-        return ResponseEntity.ok(theDataList);
+        ApiResponseData<?> response = ApiResponseData.builder().data(theDataList).message("successfully").status(HttpStatus.OK.value()).build();
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping("/estCostCapexHP")
     public ResponseEntity<?> estimated_cost_Capex_HP(@RequestBody EstimatedCostCapexHPDto estimatedCostCapexHPDto) {
         dbUpdateService.estimated_cost_Capex_HP(estimatedCostCapexHPDto);
-        return ResponseEntity.ok("update successfully");
+        ApiResponseData<?> response = ApiResponseData.builder().message("update successfully").status(HttpStatus.OK.value()).build();
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/getEstCostCapexHPList")
     public ResponseEntity<?> getTheDataListEst_Cost_Capex_HP() {
         List<EstimatedCostCapexHPDto> theDataList = dbUpdateService.getTheDataListEst_Cost_Capex_HP();
-        return ResponseEntity.ok(theDataList);
+        ApiResponseData<?> response = ApiResponseData.builder().data(theDataList).message("successfully").status(HttpStatus.OK.value()).build();
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping("/estCostOpexDeep")
     public ResponseEntity<?> estimated_cost_Opex_Deep(@RequestBody EstimatedCostOpexDeepDto estimatedCostOpexDeepDto) {
         dbUpdateService.estimated_cost_Opex_Deep(estimatedCostOpexDeepDto);
-        return ResponseEntity.ok("update successfully");
+        ApiResponseData<?> response = ApiResponseData.builder().message("update successfully").status(HttpStatus.OK.value()).build();
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/getEstCostOpexDeepList")
     public ResponseEntity<?> getTheDataListEst_Cost_Opex_Deep() {
         List<EstimatedCostOpexDeepDto> theDataList = dbUpdateService.getTheDataListEst_Cost_Opex_Deep();
-        return ResponseEntity.ok(theDataList);
+        ApiResponseData<?> response = ApiResponseData.builder().data(theDataList).message("successfully").status(HttpStatus.OK.value()).build();
+        return ResponseEntity.ok(response);
     }
     @PutMapping("/estCostOpexHP")
     public ResponseEntity<?> estimated_cost_Opex_HP(@RequestBody EstimatedCostOpexHpDto estimatedCostOpexHpDto) {
         dbUpdateService.estimated_cost_Opex_HP(estimatedCostOpexHpDto);
-        return ResponseEntity.ok("update successfully");
+
+        ApiResponseData<?> response = ApiResponseData.builder().message("update successfully").status(HttpStatus.OK.value()).build();
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/getEstCostOpexHPList")
     public ResponseEntity<?> getTheDataListEst_Cost_Opex_HP() {
         List<EstimatedCostOpexHpDto> theDataList = dbUpdateService.getTheDataListEst_Cost_Opex_HP();
-        return ResponseEntity.ok(theDataList);
+        ApiResponseData<?> response = ApiResponseData.builder().data(theDataList).message("successfully").status(HttpStatus.OK.value()).build();
+        return ResponseEntity.ok(response);
     }
     @PutMapping("/gelDataWell")
     public ResponseEntity<?> gelDataWellData(@RequestBody GelDataWellDto gelDataWellDto) {
         dbUpdateService.gelDataWell(gelDataWellDto);
-        return ResponseEntity.ok("update successfully");
+        ApiResponseData<?> response = ApiResponseData.builder().message("update successfully").status(HttpStatus.OK.value()).build();
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/getGelDataWellList")
     public ResponseEntity<?> getTheDataListGelDataWell() {
         List<GelDataWellDto> theDataList = dbUpdateService.getTheDataListGelDataWell();
-        return ResponseEntity.ok(theDataList);
+        ApiResponseData<?> response = ApiResponseData.builder().data(theDataList).message("successfully").status(HttpStatus.OK.value()).build();
+        return ResponseEntity.ok(response);
     }
 
 
@@ -93,18 +120,20 @@ public class DbUpdateController {
     @PutMapping("/heatLoadFuel")
     public ResponseEntity<?> heatLoadFuel(@RequestBody HeatLoadFuelsDto heatLoadFuelsDto) {
         dbUpdateService.heatLoadFuelsData(heatLoadFuelsDto);
-        return ResponseEntity.ok("update successfully");
+        ApiResponseData<?> response = ApiResponseData.builder().message("update successfully").status(HttpStatus.OK.value()).build();
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/getHeatLoadFuelList")
     public ResponseEntity<?> heatLoadDataList() {
         List<HeatLoadFuelsDto> theDataList = dbUpdateService.getTheHeatLoadFuelList();
-        return ResponseEntity.ok(theDataList);
+        ApiResponseData<?> response = ApiResponseData.builder().data(theDataList).message("successfully").status(HttpStatus.OK.value()).build();
+        return ResponseEntity.ok(response);
     }
 
 
 
-
-
-
 }
+
+
+
